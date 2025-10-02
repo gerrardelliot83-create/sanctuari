909ac5dfb3fae8aa6ed0ef6e02a14fb139bb4a8f
gerrardelliot83-create <gerrard.elliot83@gmail.com>
2025-10-02 15:48:49 +0530

    Fixing UploadThing

--- apps/platform/src/utils/uploadthing.js
+++ apps/platform/src/utils/uploadthing.js
@@ -1,6 +1,6 @@
-import { generateReactHelpers } from "@uploadthing/react";
+import { generateUploadButton, generateUploadDropzone } from "@uploadthing/react";
 
-// Type definition for the file router
-// This needs to match the server-side file router in api/uploadthing/core.js
-export const { UploadButton, UploadDropzone, useUploadThing } =
-  generateReactHelpers();
+// Generate typed components for the file router
+// These will use the file router defined in /api/uploadthing/core.js
+export const UploadButton = generateUploadButton();
+export const UploadDropzone = generateUploadDropzone();
